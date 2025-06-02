5048ce8a7fdb4f29fe41b3b00fc72afa66a8f2f4
victor <victorcsales@gmail.com>
2025-06-02 15:07:29 -0300

    Modificador Final

---
+++ src/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/dominio/Carro.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.javacore.Jmodificadorfinal.dominio;
+
+public class Carro {
+
+    private String nome;
+    public static final double VELOCIDADE_FINAL = 250;
+    public final Comprador COMPRADOR = new Comprador();
+
+    public final void imprime(){
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+}
