64995cbd8c389ac89b2feccb530ee060f92d1017
victor <victorcsales@gmail.com>
2025-06-15 08:07:13 -0300

    Classes Polimorfismo

---
+++ src/academy/devdojo/maratonajava/javacore/Npolimorfismo/dominio/Tomate.java
@@ -0,0 +1,24 @@
+package academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio;
+
+public class Tomate extends Produto{
+
+    private String dataValidade;
+    public static final double IMPOSTO_POR_CENTO = 0.06;
+    public Tomate(String nome, double valor) {
+        super(nome, valor);
+    }
+
+    @Override
+    public double calcularImposto() {
+        System.out.println("Calculando valor do imposto do Tomate");
+        return this.valor * IMPOSTO_POR_CENTO;
+    }
+
+    public String getDataValidade() {
+        return dataValidade;
+    }
+
+    public void setDataValidade(String dataValidade) {
+        this.dataValidade = dataValidade;
+    }
+}
